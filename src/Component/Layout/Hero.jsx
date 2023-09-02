7c5dd29c5a697570b822d598753e75d9063d6a23
ChavinduRansara <119533194+ChavinduRansara@users.noreply.github.com>
2023-09-03 00:39:48 +0530

    Finished footer

---
+++ src/Component/Layout/Hero.jsx
@@ -0,0 +1,9 @@
+import Img from '../../Images/hero.avif'
+export default function Hero() {
+    return (
+        <div>
+            <img src={`${Img}`} alt="" className="w-full h-[600px]"/>
+        </div>
+    )
+
+}
