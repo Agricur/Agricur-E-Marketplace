ce058ac6a45dc5f3b8d1296231d696e050ea50e5
ChavinduRansara <119533194+ChavinduRansara@users.noreply.github.com>
2023-09-12 21:32:40 +0530

    update files

--- src/Component/Categories/Vegetables.jsx
+++ src/Component/Categories/Vegetables.jsx
@@ -32,7 +32,7 @@ export default function AllCategories(user_id) {
     };
   
     return (
-      <div className="bg-[#d9eada] p-20">
+      <div className="bg-[#d9eada] pt-28">
         <div className="mx-auto max-w-2xl px-4 py-8 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
           <h2 className="text-2xl font-bold tracking-tight text-gray-900">
             Vegetables
