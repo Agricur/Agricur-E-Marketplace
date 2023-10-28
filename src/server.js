72f09a78639a130ca2895f008cede1ca82a3a9f4
Tishani Rathnasiri <tishani.20@cse.mrt.ac.lk>
2023-10-28 22:22:15 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,4 @@
-export const server = "http://localhost:5000";
+
+// export const server = "http://192.168.35.248:5000";
+//export const server = "http://localhost:5000";
+export const server = "https://backend-3zfs.onrender.com"
