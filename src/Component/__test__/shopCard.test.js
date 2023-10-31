a396fb41453112c083c7d40d08b37fed2ed7b01e
ChavinduRansara <119533194+ChavinduRansara@users.noreply.github.com>
2023-10-31 23:34:47 +0530

    implement testing

---
+++ src/Component/__test__/shopCard.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter as Router
+import ShopCard from '../ShopView/ShopCard';
+
+test('should render the ShopCard component with correct data', () => {
+  const shopData = {
+    shop_id: 1,
+    name: 'Example Shop',
+    image: 'example.jpg',
+    rating: 4.5,
+  };
+
+  render(
+    <Router>
+      <ShopCard {...shopData} />
+    </Router>
+  );
+
+  const shopName = screen.getByText('Example Shop');
+  const rating = screen.getByText('Rating:');
+
+  expect(shopName).toBeInTheDocument();
+  expect(rating).toBeInTheDocument();
+});
+
+test('should have a link to the shop home', () => {
+    const shopData = {
+      shop_id: 1,
+      name: 'Example Shop',
+      image: 'example.jpg',
+      rating: 4.5,
+    };
+  
+    render(
+      <Router>
+        <ShopCard {...shopData} />
+      </Router>
+    );
+  
+    const shopLink = screen.getByRole('link', { name: (content, element) => content.startsWith('Example Shop') });
+  
+    expect(shopLink).toHaveAttribute('href', '/ShopHome/1');
+  });
+   
